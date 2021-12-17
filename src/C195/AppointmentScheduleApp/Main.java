c47ebb2f3d5e28fb74adaa00720afbd02b4bc9be
s-prey <prey.steven@gmail.com>
2021-12-16 22:40:27 -0800

    Initial commit

--- src/C195/AppointmentScheduleApp/Main.java
+++ src/C195/AppointmentScheduleApp/Main.java
@@ -1,8 +1,23 @@
 package C195.AppointmentScheduleApp;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        launch(args);
     }
 }
