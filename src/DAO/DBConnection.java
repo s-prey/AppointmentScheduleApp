d4da82ff0ec80a4d756d4b8c8fcee1181d96eb19
s-prey <prey.steven@gmail.com>
2022-01-03 12:57:48 -0800

    DBConnection method

--- src/DAO/DBConnection.java
+++ src/DAO/DBConnection.java
@@ -10,16 +10,28 @@ public class DBConnection {
     private static final String DB_URL = "jdbc:myql://localhost:3306/"+databaseName;
     private static final String username = "sqlUser";
     private static final String password = "Passw0rd!";
+    private static Connection conn = null;
+
+    private static final String MYSQLJBCDriver = "com.mysql.jdbc.Driver";
 
 
-
-    static Connection conn;
-
-    public static void  makeConnection() throws ClassNotFoundException, SQLException, Exception {
-        conn = (Connection) DriverManager.getConnection(DB_URL, username, password);
+    public static Connection startConnection() { //throws ClassNotFoundException, SQLException, Exception {
+        try {
+            Class.forName(MYSQLJBCDriver);
+            conn = (Connection) DriverManager.getConnection(DB_URL, username, password);
+            System.out.println("Connection successful");
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return conn;
     }
 
-    public static void closeConnection() throws ClassNotFoundException, SQLException, Exception {
-        conn.close();
+    public static void closeConnection() {//throws ClassNotFoundException, SQLException, Exception {
+        try {
+            conn.close();
+            System.out.println("Connection closed");
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
