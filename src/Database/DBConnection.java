657edacb5392cf35f0bb39b703321836c06a6697
s-prey <prey.steven@gmail.com>
2022-03-13 21:32:49 -0700

    Added java doc notes for all classes and models.

--- src/Database/DBConnection.java
+++ src/Database/DBConnection.java
@@ -4,22 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
+/** This class is used to contain methods for connecting to the client_schedule database.*/
 public class DBConnection {
     private static final String databaseName = "client_schedule";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/"+databaseName;
     private static final String username = "sqlUser";
     private static final String password = "Passw0rd!";
-   private static Connection conn = null;
+    private static Connection conn = null;
     private static final String MYSQLJBCDriver = "com.mysql.jdbc.Driver";
 
-
+    /** This is the start connection method.
+     This method establishes an initial connection to the database.
+     @return Returns database connection URL, database connection username, and database connection password
+     */
     public static Connection startConnection() {
         try {
             Class.forName(MYSQLJBCDriver);
             conn = DriverManager.getConnection(DB_URL, username, password);
-
-            System.out.println("Connection successful");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -29,12 +30,17 @@ public class DBConnection {
         return conn;
     }
 
-
+    /** This is the get connection method.
+     This method returns the established connection to the database.
+     @return Returns established database connection URL, database connection username, and database connection password
+     */
     public static Connection getConnection() {
         return conn;
     }
 
-
+    /** This is the close connection method.
+     This method closes the database connection.
+     */
     public static void closeConnection() {
         try {
             conn.close();
